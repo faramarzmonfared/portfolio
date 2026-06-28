04b6dbd8f048d08a9736941449c4be42c83cd6aa
Faramarz <faramarzmonfared@gmail.com>
2026-06-28 16:11:44 +0330

    Switch database from SQLite to PostgreSQL

--- config/settings/base.py
+++ config/settings/base.py
@@ -13,6 +13,8 @@ from dotenv import load_dotenv
 from pathlib import Path
 import os
 
+import dj_database_url
+
 
 load_dotenv()
 
@@ -108,10 +110,9 @@ WSGI_APPLICATION = 'config.wsgi.application'
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    "default": dj_database_url.config(
+        default=os.environ["DATABASE_URL"]
+    )
 }
 
 
