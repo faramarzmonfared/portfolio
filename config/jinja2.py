f570a64a521aa3c141212d43bd76ea0a53e98cd6
Faramarz <faramarzmonfared@gmail.com>
2026-06-28 14:12:04 +0330

    Add markdown rendering filter with typography style

--- config/jinja2.py
+++ config/jinja2.py
@@ -2,10 +2,25 @@
 
 from typing import Any
 
+import markdown as markdown_lib
 from django.templatetags.static import static
 from django.urls import reverse
+from django.utils.safestring import mark_safe
 from django.utils.translation import gettext
 from jinja2 import Environment
+
+
+def render_markdown(text: str) -> str:
+    """Convert markdown text to safe HTML.
+
+    Args:
+        text: Raw markdown content.
+
+    Returns:
+        Rendered HTML marked as safe for template output.
+    """
+    html = markdown_lib.markdown(text, extensions=["fenced_code", "tables"])
+    return mark_safe(html)
 
 
 def environment(**options: Any) -> Environment:
@@ -25,4 +40,5 @@ def environment(**options: Any) -> Environment:
             "_": gettext,
         }
     )
+    env.filters["markdown"] = render_markdown
     return env
